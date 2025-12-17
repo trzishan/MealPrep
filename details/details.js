713ddb250f6ede7754daa294fed38d64038147dc
Tahmid Zishan <tahmidzishan53@gmail.com>
2025-12-17 21:42:11 +0600

    Crappy Serach for details page added

--- details/details.js
+++ details/details.js
@@ -10,7 +10,7 @@ async function getReq(fetchEndpoint){
     const meal = data.meals[0];
     console.log(meal);
 
-    const detail = document.getElementById('main');
+    const detail = document.getElementById('details');
     const intro = document.createElement('div');
     intro.setAttribute('class', 'intro');
     intro.innerHTML = `
@@ -60,3 +60,68 @@ async function getReq(fetchEndpoint){
 }
 
 getReq(fetchEndpoint.toString());
+
+
+
+
+
+
+// Crappy Search
+
+const searchBox = document.getElementById('searchContainer');
+searchBox.addEventListener('submit', async function (event) {
+  // API endpoint for retrieving all meals from TheMealDB
+  // https://www.themealdb.com/api/json/v1/1/search.php?s
+  
+  event.preventDefault()
+  const searchTerm = document.getElementById('searchBox').value
+  console.log(searchTerm)
+
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+  );
+  
+  // Parse the response as JSON to get the meal objects
+  const data = await res.json();
+
+  // Get the container element where meal cards will be inserted
+  const cardList = document.getElementById("main");
+  cardList.innerHTML = '';
+
+  // Clear out the details
+  document.getElementById("details").innerHTML = '';
+
+  
+  // Iterate through each meal in the API response
+  data.meals.forEach((meal) => {
+    // Create a new div element to serve as the meal card
+    const card = document.createElement("div");
+    
+    // Add the "card" CSS class to style the card element
+    card.setAttribute("class", "card");
+
+    
+    // Set the inner HTML of the card (currently empty, to be populated with meal details)
+    card.innerHTML = `
+            
+                <div class="image">
+                    <img src="${meal.strMealThumb}" alt="Photo of ${meal.strMeal}">
+                </div>
+                <div class="cardText">
+                    <div class="khanarNam"><b>Name:</b> ${meal.strMeal}</div>
+                    <div class="type"><b>Category:</b> ${meal.strCategory}</div>
+                    <a href="details/?id=${meal.idMeal}"><button class="simorButton"> See Details</button></a>
+                </div>
+            
+        `;
+    
+    // Append the card to the cardList container to display it on the page
+    cardList.appendChild(card);
+  });
+
+  // Log the fetched data to the console for debugging purposes
+  console.log(data);
+})
+
+
+
